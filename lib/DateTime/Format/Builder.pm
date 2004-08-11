1a86e7d58b5365c6236d0c9a9b1da4fb0c73e2f2
Dave Rolsky <autarch@urth.org>
2004-08-11 07:11:14 +0000

    new version #

--- lib/DateTime/Format/Builder.pm
+++ lib/DateTime/Format/Builder.pm
@@ -17,7 +17,7 @@ use Params::Validate qw(
 use vars qw( $VERSION %dispatch_data );
 
 my $parser = 'DateTime::Format::Builder::Parser';
-$VERSION = '0.7804';
+$VERSION = '0.7805';
 
 # Developer oriented methods
 
