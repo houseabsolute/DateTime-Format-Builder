b5fb45bc13c7ecc2691d319420de8d3ebd6d5b15
Dave Rolsky <autarch@urth.org>
2004-09-09 14:06:15 +0000

    urgh, I already release 0.7805.  This will be 0.7806

--- lib/DateTime/Format/Builder.pm
+++ lib/DateTime/Format/Builder.pm
@@ -17,7 +17,7 @@ use Params::Validate qw(
 use vars qw( $VERSION %dispatch_data );
 
 my $parser = 'DateTime::Format::Builder::Parser';
-$VERSION = '0.7805';
+$VERSION = '0.7806';
 
 # Developer oriented methods
 
